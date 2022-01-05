c9878573faaec4c34e9be7769298fa8caf40744b
filmee24 <filmee24@gmail.com>
2022-01-05 14:30:18 +0100

    Add ParserTests

--- UnitTests/LexerTests.cs
+++ UnitTests/LexerTests.cs
@@ -1,67 +1,66 @@
 using EbnfParserGenerator.Ebnf;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTests
+namespace UnitTests;
+
+[TestClass]
+public class LexerTests
 {
-    [TestClass]
-    public class LexerTests
+    [TestMethod]
+    public void Identifiers_Should_Pass()
     {
-        [TestMethod]
-        public void Identifiers_Should_Pass()
-        {
-            AssertToken("hello123", TokenType.Identifier);
-            AssertToken("aBc", TokenType.Identifier);
-        }
+        AssertToken("hello123", TokenType.Identifier);
+        AssertToken("aBc", TokenType.Identifier);
+    }
 
-        [TestMethod]
-        public void Keywords_Should_Pass()
-        {
-            AssertToken("token", TokenType.TokenKeyword);
-            AssertToken("type", TokenType.TypeKeyword);
-            AssertToken("grammar", TokenType.GrammarKeyword);
-            AssertToken("for", TokenType.For);
-        }
+    [TestMethod]
+    public void Keywords_Should_Pass()
+    {
+        AssertToken("token", TokenType.TokenKeyword);
+        AssertToken("type", TokenType.TypeKeyword);
+        AssertToken("grammar", TokenType.GrammarKeyword);
+        AssertToken("for", TokenType.For);
+    }
 
-        [TestMethod]
-        public void Number_Should_Pass()
-        {
-            AssertToken("123", TokenType.Number);
-        }
+    [TestMethod]
+    public void Number_Should_Pass()
+    {
+        AssertToken("123", TokenType.Number);
+    }
 
-        [TestMethod]
-        public void String_Should_Pass()
-        {
-            AssertToken("'hello world 123'", TokenType.StringLiteral);
-            AssertToken("\"hello world 123\"", TokenType.StringLiteral);
-        }
+    [TestMethod]
+    public void String_Should_Pass()
+    {
+        AssertToken("'hello world 123'", TokenType.StringLiteral);
+        AssertToken("\"hello world 123\"", TokenType.StringLiteral);
+    }
 
-        [TestMethod]
-        public void Symbol_Should_Pass()
-        {
-            AssertToken("->", TokenType.GoesTo);
-            AssertToken(";", TokenType.Semicolon);
-            AssertToken("|", TokenType.Pipe);
-            AssertToken("+", TokenType.Plus);
-            AssertToken("*", TokenType.Star);
-            AssertToken("?", TokenType.Question);
-            AssertToken("!", TokenType.Exclamation);
-            AssertToken("(", TokenType.OpenParen);
-            AssertToken(")", TokenType.CloseParen);
-            AssertToken("{", TokenType.OpenCurly);
-            AssertToken("}", TokenType.CloseCurly);
-        }
+    [TestMethod]
+    public void Symbol_Should_Pass()
+    {
+        AssertToken("->", TokenType.GoesTo);
+        AssertToken(";", TokenType.Semicolon);
+        AssertToken("|", TokenType.Pipe);
+        AssertToken("+", TokenType.Plus);
+        AssertToken("*", TokenType.Star);
+        AssertToken("?", TokenType.Question);
+        AssertToken("!", TokenType.Exclamation);
+        AssertToken("(", TokenType.OpenParen);
+        AssertToken(")", TokenType.CloseParen);
+        AssertToken("{", TokenType.OpenCurly);
+        AssertToken("}", TokenType.CloseCurly);
+    }
 
-        private static void AssertToken(string src, TokenType tokenType)
-        {
-            var lexer = new Lexer();
+    private static void AssertToken(string src, TokenType tokenType)
+    {
+        var lexer = new Lexer();
 
-            var tokens = lexer.Tokenize(src);
+        var tokens = lexer.Tokenize(src);
 
-            Assert.IsTrue(lexer.Messages.Count == 0);
-            Assert.AreEqual(2, tokens.Count);
+        Assert.IsTrue(lexer.Messages.Count == 0);
+        Assert.AreEqual(2, tokens.Count);
 
-            Assert.AreEqual(tokens[0].Type, tokenType);
-            Assert.AreEqual(tokens[1].Type, TokenType.EOF);
-        }
+        Assert.AreEqual(tokens[0].Type, tokenType);
+        Assert.AreEqual(tokens[1].Type, TokenType.EOF);
     }
 }
---
+++ UnitTests/ParserTests.cs
@@ -0,0 +1,69 @@
+﻿using EbnfParserGenerator.Ebnf;
+using EbnfParserGenerator.Ebnf.AST;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTests;
+
+[TestClass]
+public class ParserTests
+{
+    [TestMethod]
+    public void SimpleToken_Should_Pass()
+    {
+        var src = "token 'hello';";
+        var p = Parser.Parse(src);
+
+        AssertNoError(p);
+
+        Assert.IsTrue(p.Tree is Block b && b.Body.OfType<TokenSymbolNode>().Any());
+
+        var body = ((Block)p.Tree).Body;
+
+        var node = body[0];
+
+        Assert.IsTrue(node is TokenSymbolNode t && t.Symbol == "hello" && t.Name == "Hello");
+    }
+
+    [TestMethod]
+    public void Type_Should_Pass()
+    {
+        var src = "type Expr -> | Literal(Value : Object);";
+        var p = Parser.Parse(src);
+
+        AssertNoError(p);
+
+        Assert.IsTrue(p.Tree is Block b && b.Body.OfType<TypeDeclaration>().Any());
+
+        var body = ((Block)p.Tree).Body;
+
+        var node = body[0];
+
+        Assert.IsTrue(node is TypeDeclaration t && t.Name == "Expr" && t.Block.Body.Count == 1);
+    }
+
+    [TestMethod]
+    public void Type_Without_Property_Should_Pass()
+    {
+        var src = "type Expr -> | Literal();";
+        var p = Parser.Parse(src);
+
+        AssertNoError(p);
+
+        Assert.IsTrue(p.Tree is Block b && b.Body.OfType<TypeDeclaration>().Any());
+
+        var body = ((Block)p.Tree).Body;
+
+        var node = body[0];
+
+        Assert.IsTrue(node is TypeDeclaration t && t.Name == "Expr" && t.Block.Body.Count == 1);
+    }
+
+    private void AssertNoError((ASTNode Tree, List<Message> Messages) p)
+    {
+        Assert.IsTrue(p.Messages.Count == 0);
+        Assert.IsNotNull(p.Tree);
+        Assert.IsTrue(p.Tree is Block b && b.Body.Count > 0);
+    }
+}
